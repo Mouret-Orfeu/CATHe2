509dd39eba4a56977e36e9203c3828812b0af9c8
Mouret <orfeu8@live.fr>
2024-06-21 18:33:39 +0100

    chore: first test run of prediction with cathe, failure, requirements and gitignore update

--- src/cathe-predict/fasta_to_ds.py
+++ src/cathe-predict/fasta_to_ds.py
@@ -1,10 +1,12 @@
 import pandas as pd 
 from Bio import SeqIO
 
+fasta_file_path = "/home/orfeu/Documents/Internship/Work/sandbag_storage/uniprotkb_accession_A0A0C5B5G6_2024_06_20.fasta"
+
 seq = []
 desc = []
 
-with open("proteins.fa") as handle:
+with open(fasta_file_path) as handle:
     for record in SeqIO.parse(handle, "fasta"):
         seq.append(str(record.seq))
         desc.append(record.description)
@@ -14,3 +16,5 @@ df = pd.DataFrame(list(zip(desc, seq)),
 
 print(df)
 df.to_csv('Dataset.csv')
+
+
