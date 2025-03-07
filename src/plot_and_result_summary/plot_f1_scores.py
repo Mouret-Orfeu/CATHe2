9d22c8a2db118f15807038eb4472fa6ea6cfa74c
Orfeu <orfeu8@live.fr>
2025-03-07 13:42:09 +0100

    feat: some result updates with F1 score and accuracy std

--- src/plot_and_result_summary/plot_f1_scores.py
+++ src/plot_and_result_summary/plot_f1_scores.py
@@ -235,8 +235,8 @@ models_to_plot = ['ProtT5', 'ESM2', 'Ankh_large', 'Ankh_base', 'ProstT5_full', '
 
 # example usage function plot_all_f1_scores
 
-# list_model_to_show = ['ProtT5_new', 'ProtT5', 'ESM2', 'Ankh_large', 'Ankh_base', 'ProstT5_full', 'ProstT5_half', 'TM_Vec']
-list_model_to_show = ['ProstT5_full']
+list_model_to_show = ['ProtT5_new', 'ProtT5', 'ESM2', 'Ankh_large', 'Ankh_base', 'ProstT5_full', 'ProstT5_half', 'TM_Vec']
+
 
 plot_all_f1_scores(df, list_model_to_show)
 
